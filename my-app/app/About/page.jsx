8c1149b81660863c109e6f3f10751707aebaff56
rezvan khalili <rezvan.kh.1999@gmail.com>
2025-09-21 10:11:35 +0330

    complete the about and home page

--- my-app/app/About/page.jsx
+++ my-app/app/About/page.jsx
@@ -1,15 +1,26 @@
-
+import style from "./about.module.css";
 
 export default function page() {
   return (
-    <div>
-      <h3>"About us"</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. In fugiat
-        maxime porro labore fugit sequi, soluta illo earum eius doloremque
-        voluptatibus reprehenderit tenetur, sint, ipsa corrupti omnis voluptatum
-        iure voluptate.
-      </p>
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <h3>"About us"</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. In fugiat
+          maxime porro labore fugit sequi, soluta illo earum eius doloremque
+          voluptatibus reprehenderit tenetur, sint, ipsa corrupti omnis
+          voluptatum iure voluptate.
+          <br />
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt earum
+          qui, eos sed dolor sequi quibusdam a tempore laborum esse, dolorum
+          excepturi. Culpa consectetur quas quo totam hic perferendis? Accusamus
+          nulla perferendis vero eaque vel quaerat et asperiores iste at.
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          dicta hic soluta? Laudantium, eaque quia? Vero ipsam tempora unde
+          excepturi!
+        </p>
+      </div>
     </div>
   );
 }
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,6 +2,7 @@ import style from "./page.module.css";
 
 export default function Home() {
   return (
+    <div className={style.wrapper}>
     <div className={style.container}>
       <h4>
         welcome to home page.
@@ -9,6 +10,6 @@ export default function Home() {
         We have prepared the best products with excellent quality for you. Shop
         and enjoy 🥂
       </h4>
-    </div>
+    </div></div>
   );
 }
